03f9354dfef5aeef79ab17dd37c2ecb77632cc95
loannjui <loann.juillerat@heig-vd.ch>
2026-02-02 16:14:54 +0100

    should work

--- src/modules/Color.js
+++ src/modules/Color.js
@@ -9,14 +9,14 @@ export default class Color {
     this.#element = this.#generateElement;
   }
   #generateElement() {
-    const colorElement = document.querySelector("div");
+ const colorElement = document.createElement("div");
     colorElement.classList.add("color");
     colorElement.dataset.color = this.#hex;
     colorElement.style.backgroundColor = this.#hex;
-    const pElement = document.querySelector("p");
+    const pElement = document.createElement("p");
     pElement.style.color = this.#hsl[2] < 60 ? "#fff" : "#000";
     pElement.textContent = `${this.#hex}`;
-    colorElement.insertAdjacentHTML("beforeend", pElement);
+    colorElement.appendChild(pElement);
     return colorElement;
   }
   display(parentElement){
